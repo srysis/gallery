f9262843bbd88e5dedfc205119ad3be11bf2bd62
srysis <jashandenis@gmail.com>
2025-01-09 17:34:13 +0100

    rewritten a logic behind 'switching images in fullscreen' functionality; now it is being handled by a state, data attribute and 'useEffect' hook

--- src/components/GalleryFullscreen.jsx
+++ src/components/GalleryFullscreen.jsx
@@ -2,37 +2,48 @@ import React from "react"
 
 import cross from "../files/cross.svg"
 
-function GalleryFullscreen({toggleFullscreenFunction, currentImage}) {
-	function switchToNextImage() {
-		let current_image = document.querySelector("img.current_image");
+function GalleryFullscreen({toggleFullscreenFunction, passedImageIndex}) {
+	const [currentImageIndex, setCurrentImageIndex] = React.useState(passedImageIndex);
 
-		if (current_image.parentElement.nextElementSibling != null) {
-			current_image.classList.toggle("current_image");
-			current_image.parentElement.nextElementSibling.firstElementChild.classList.toggle("current_image");
+	let image = document.querySelector(`img[data-index="${passedImageIndex}"]`).getAttribute("src");
+
+	React.useEffect(() => {
+		let requested_element = document.querySelector(`img[data-index="${currentImageIndex}"]`)
+		
+		if (requested_element != null) {
+			let requested_image = requested_element.getAttribute("src");
 
 			let image_container = document.querySelector("img.image_fullscreen");
-			image_container.setAttribute("src", document.querySelector("img.current_image").getAttribute("src"));
+			image_container.setAttribute("src", requested_image);
+		}
+	}, [currentImageIndex]);
+
+	function switchToNextImage() {
+		let temp_index = currentImageIndex;
+
+		if (temp_index + 1 > 20) {
+			setCurrentImageIndex(temp_index);
+		} else {
+			setCurrentImageIndex(temp_index + 1);
 		}
 	}
 
 	function switchToPreviousImage() {
-		let current_image = document.querySelector("img.current_image");
+		let temp_index = currentImageIndex;
 
-		if (current_image.parentElement.previousElementSibling != null) {
-			current_image.classList.toggle("current_image");
-			current_image.parentElement.previousElementSibling.firstElementChild.classList.toggle("current_image");
-
-			let image_container = document.querySelector("img.image_fullscreen");
-			image_container.setAttribute("src", document.querySelector("img.current_image").getAttribute("src"));
+		if (temp_index - 1 <= 0) {
+			setCurrentImageIndex(temp_index)
+		} else {
+			setCurrentImageIndex(temp_index - 1);
 		}
 	}
 
 	return(
 		<div id="image_fullscreen_overlay_container" className="active" >
 			<button id="prev_photo" onClick={() => switchToPreviousImage()} >&lt;</button> 
-			<img className="image_fullscreen" src={currentImage} />
+			<img className="image_fullscreen" src={image} alt="" />
 			<button id="next_photo" onClick={() => switchToNextImage()} >&gt;</button>
-			<button id="close_button" onClick={() => { toggleFullscreenFunction(false); document.querySelector('img.current_image').classList.remove("current_image") }} ><img src={cross} /></button>
+			<button id="close_button" onClick={() => { toggleFullscreenFunction(false) }} ><img src={cross} /></button>
 		</div>
 	)
 }
